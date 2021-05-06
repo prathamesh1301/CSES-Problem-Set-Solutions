1bcb07311e0f06773b1d3fa55831717315d21f7e
Prathamesh Autade <57824845+prathamesh1301@users.noreply.github.com>
2021-05-06 16:13:23 +0530

    Introductory Problems

---
+++ Increasing Array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<stack>
+#include<vector>
+#include<ctype.h>
+#include<algorithm>
+#include <unordered_map>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    long long a[n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    long long ct=0;
+    for(int i=0;i<n-1;i++){
+        if(a[i]>a[i+1]){
+            long long diff=a[i]-a[i+1];
+            ct+=diff;
+            a[i+1]=a[i];
+            }
+ 
+    }
+    cout<<ct;
+    
